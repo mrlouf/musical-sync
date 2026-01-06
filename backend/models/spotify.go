91be1293fd06b9e9baeeed12b0b7f490be4f27b1
Nicolas <ponchon.nicolas@gmail.com>
2026-01-06 14:55:21 +0100

    Commented the structures used to paginate the Spotify playlist

--- backend/models/spotify.go
+++ backend/models/spotify.go
@@ -6,6 +6,12 @@ type SpotifyTrackNumberResponse struct {
 	} `json:"tracks"`
 }
 
+/* 
+	This struct will contain 100 items per request, since Spotify's API
+	limits the number of items returned in a single request to 100.
+	We need to paginate through the results to get all tracks and
+	store them in a slice of SpotifyPlaylistItem, which is defined below.
+*/
 type SpotifyPlaylistItem struct {
     Track struct {
         Name    string `json:"name"`
